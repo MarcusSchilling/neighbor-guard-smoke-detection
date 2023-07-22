2ee6895a1d1d27f9f34ccf2802f5dddb41c1a4aa
Marcus Schilling <marcus@5-schillinge.de>
2023-07-22 22:10:22 +0200

    Introduced Telegram Bot

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,43 @@
+#define WIFI_SSID "$SSID"
+#define WIFI_PASS "$PASSWORD"
+#define BOT_TOKEN "$BOT_Token"
+#define CHAT_ID "$CHAT_ID"
+
+#include <FastBot.h>
+FastBot bot(BOT_TOKEN);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); // Initialize the Serial monitor with the baud rate 115200
+  connectWiFi();
+  setupBot();
   while (true) {
-    delay(100);
     loop();
+    delay(15000);
   }
 }
 
+void setupBot() {
+  bot.setChatID(CHAT_ID);
+  bot.attach(newMsg);
+}
+
+void connectWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (millis() > 15000) ESP.restart();
+    Serial.println("Not Connected");
+  }
+  Serial.println("Connected");
+}
+
+void newMsg(FB_msg& msg) {
+  Serial.println(msg.toString());
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello, World!"); // Print "Hello, World!" to the Serial monitor
+  // put your main code here, to run repeatedly:i
+  bot.sendMessage("Hello, World!"); // Print "Hello, World!" to the Serial monitor
 }
