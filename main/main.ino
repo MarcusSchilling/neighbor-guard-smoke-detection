17fd65a624b832dda0ef12809847d084b94df801
Marcus Schilling <marcus@5-schillinge.de>
2023-07-22 23:37:50 +0200

    Refactored God Class to second file that contians Telegram Bot Class and Wifi Handler Class

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,12 @@
-#include "credentials.h"
-#include <FastBot.h>
+#include "telegram_bot.cpp"
 
-FastBot bot(BOT_TOKEN);
+TelegramBot telegramBot;
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200); // Initialize the Serial monitor with the baud rate 115200
-  connectWiFi();
-  setupBot();
+  telegramBot.initialize();
   while (true)
   {
     loop();
@@ -16,35 +14,7 @@ void setup()
   }
 }
 
-void setupBot()
-{
-  bot.setChatID(CHAT_ID);
-  bot.attach(newMsg);
-}
-
-void connectWiFi()
-{
-  Serial.print(WIFI_SSID);
-  Serial.print(WIFI_PASS);
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-    if (millis() > 15000)
-      ESP.restart();
-    Serial.println("Not Connected");
-  }
-  Serial.println("Connected");
-}
-
-void newMsg(FB_msg &msg)
-{
-  Serial.println(msg.toString());
-}
-
 void loop()
 {
-  // put your main code here, to run repeatedly:i
-  bot.sendMessage("Hello, World!"); // Print "Hello, World!" to the Serial monitor
+  telegramBot.sendMessage("Hello");
 }
---
+++ main/telegram_bot.cpp
@@ -0,0 +1,50 @@
+#define telegram_bot
+#include <FastBot.h>
+#include "credentials.h"
+
+class WifiConnection
+{
+    public:
+
+    bool hasConnection()
+    {
+        return WiFi.status() != WL_CONNECTED;
+    }
+
+
+    void connectWiFi()
+    {
+        WiFi.begin(WIFI_SSID, WIFI_PASS);
+        while (hasConnection())
+        {
+          delay(500);
+          Serial.print(".");
+          if (millis() > 15000)
+            ESP.restart();
+        }
+    }
+    
+};
+
+class TelegramBot
+{
+private:
+    FastBot fastBot{BOT_TOKEN};
+    WifiConnection wifiConnection{};
+public:
+    TelegramBot()
+    {
+        fastBot.setChatID(CHAT_ID);
+    }
+
+    void initialize()
+    {
+      wifiConnection.connectWiFi();
+    }
+
+    void sendMessage(String message)
+    {
+        fastBot.sendMessage(message);
+    }
+
+};
