f42251025169cc09cd64d88a548593de8a94fa84
Marcus Schilling <marcus@5-schillinge.de>
2023-07-22 22:33:51 +0200

    Added way to include credentials without having them in the repo

--- main/main.ino
+++ main/main.ino
@@ -1,43 +1,50 @@
-#define WIFI_SSID "$SSID"
-#define WIFI_PASS "$PASSWORD"
-#define BOT_TOKEN "$BOT_Token"
-#define CHAT_ID "$CHAT_ID"
-
+#include "credentials.h"
 #include <FastBot.h>
+
 FastBot bot(BOT_TOKEN);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200); // Initialize the Serial monitor with the baud rate 115200
   connectWiFi();
   setupBot();
-  while (true) {
+  while (true)
+  {
     loop();
     delay(15000);
   }
 }
 
-void setupBot() {
+void setupBot()
+{
   bot.setChatID(CHAT_ID);
   bot.attach(newMsg);
 }
 
-void connectWiFi() {
+void connectWiFi()
+{
+  Serial.print(WIFI_SSID);
+  Serial.print(WIFI_PASS);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
-    if (millis() > 15000) ESP.restart();
+    if (millis() > 15000)
+      ESP.restart();
     Serial.println("Not Connected");
   }
   Serial.println("Connected");
 }
 
-void newMsg(FB_msg& msg) {
+void newMsg(FB_msg &msg)
+{
   Serial.println(msg.toString());
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:i
   bot.sendMessage("Hello, World!"); // Print "Hello, World!" to the Serial monitor
 }
