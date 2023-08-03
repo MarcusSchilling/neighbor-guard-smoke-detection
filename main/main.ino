d3369280b77960b64d014af5af18773aa7703699
EH <44804677+EtienneHenger@users.noreply.github.com>
2023-08-03 23:10:43 +0200

    Initialize MQ135 Sensor to send ppm and resistance readout to telegram bot

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,10 @@
 #include "telegram_bot.cpp"
 
+// Sensor MQ135
+#include "MQ135.h"
+const int ANALOGPIN=0;
+MQ135 gasSensor = MQ135(ANALOGPIN);
+
 TelegramBot telegramBot;
 
 void setup()
@@ -16,5 +21,14 @@ void setup()
 
 void loop()
 {
-  telegramBot.sendMessage("Hello");
+  // read the input on analog pin 0:
+  float ppm = gasSensor.getPPM();
+  float resistance = gasSensor.getResistance();
+  // print out the value you read:
+  Serial.print("\t Resistance: ");
+  Serial.print(resistance);
+  Serial.print("\t PPM: ");
+  Serial.print(ppm);
+  // send sensor readouts to telegram bot
+  telegramBot.sendMessage(String("Sensor MQ135 Readout\n PPM: " + String(ppm) + "\n RAW Resistance: " + String(resistance)));
 }
