34c3fc0c6c5a3af141aeae0d40dac005bc792a4e
Marcus Schilling <marcus@5-schillinge.de>
2023-08-13 23:53:57 +0200

    Now only a notification takes place if PM is over 10 and time is between 9 o'clock and 23 o'lock

--- main/telegram_notifier.cpp
+++ main/telegram_notifier.cpp
@@ -2,11 +2,12 @@
 #define TELEGRAM_NOTIFIER
 #include "observer.cpp"
 #include "telegram_bot.cpp"
-
+#include "notification_policy.cpp"
 class TelegramNotifier : public Observer
 {
 public:
     TelegramBot telegramBot;
+    NotificationPolicy notificationPolicy;
 
     TelegramNotifier()
     {
@@ -15,7 +16,7 @@ public:
 
     void update(const Measurement &measurement) override
     {
-        if (measurement.getSensor() == SensorType::MQ135)
+        if (measurement.getSensor() == SensorType::MQ135 && notificationPolicy.shouldNotify(measurement))
         {
             telegramBot.sendMessage(String("Sensor MQ135 \nPPM: " + String(measurement.getPM()) + "\nRAW Resistance: " + String(measurement.getResistance())));
         }
