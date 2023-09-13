74c1141fc77f9f07adf72082754cea0000d4579b
Marcus Schilling <marcus@5-schillinge.de>
2023-09-13 16:01:59 +0200

    Extracted Wifi Connection from Telegram Bot and added OTA feature

---
+++ main/wifi_connection.h
@@ -0,0 +1,28 @@
+#ifndef WIFI_CONNECTION_H
+#define WIFI_CONNECTION_H
+#include "credentials.h"
+#include <ESP8266WiFi.h>
+
+class WifiConnection
+{
+public:
+    bool hasConnection()
+    {
+        return WiFi.status() != WL_CONNECTED;
+    }
+
+
+
+    void connectWiFi()
+    {
+        WiFi.begin(WIFI_SSID, WIFI_PASS);
+        while (hasConnection())
+        {
+            delay(500);
+            Serial.print(".");
+            if (millis() > 15000)
+                ESP.restart();
+        }
+    }
+};
+#endif
