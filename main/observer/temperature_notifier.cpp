097f863b53372011933b3ca021b07c063b7eb0fd
Marcus Schilling <marcus@5-schillinge.de>
2023-09-13 19:04:12 +0200

    Refactor folder structure

--- main/observer/temperature_notifier.cpp
+++ main/observer/temperature_notifier.cpp
@@ -1,8 +1,9 @@
 #ifndef TEMPERATURE_NOTIFIER_CPP
 #define TEMPERATURE_NOTIFIER_CPP
 #include "observer.cpp"
-#include "notification_policy.cpp"
-#include "telegram_bot.cpp"
+#include "../notification/notification_policy.cpp"
+#include "../notification/telegram_bot.cpp"
+#include "../domain/measurement.hpp"
 
 class TemperatureNotifier : public Observer
 {
