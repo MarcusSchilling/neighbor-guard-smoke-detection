4942237e19bf02fc37bac5dcb89e32e04335d4ee
Marcus Schilling <marcus@5-schillinge.de>
2023-09-13 17:04:58 +0200

    OTA complete]

--- main/main.ino
+++ main/main.ino
@@ -1,21 +1,26 @@
 #ifndef MAIN
 #define MAIN
 
-#include "measurement.hpp"
 #include "observer.cpp"
+
+// Networking
 #include "telegram_notifier.cpp"
+#include "wifi_connection.h"
+#include "ota.h"
+
+// Configuration Files
 #include "constants.h"
+#include "credentials.h"
+
+// General Sensor Configuration
+#include "measurement.hpp"
 
 // Sensor MQ135
 #include <MQ135.h>
-// MQ135 gasSensor = MQ135(ANALOGPIN); // uncalibrated sensor
 MQ135 gasSensor = MQ135(ANALOGPIN, RZEROCALIBRATION); // calibrated sensor
 
 // Sensor DHT11
 #include <DHT.h>
-#include "credentials.h"
-#include "wifi_connection.h"
-#include <ArduinoOTA.h>
 
 WifiConnection wifiConnection{};
 DHT hygroSensor = DHT(DIGITALPIN, DHTTYPE);
@@ -34,22 +39,8 @@ void setup()
   // put your setup code here, to run once:
   Serial.begin(BUADRATE);
   wifiConnection.connectWiFi();
-  ArduinoOTA.setHostname("smoke-guard");
-  ArduinoOTA.onStart([]()
-                     { Serial.println("Start"); });
-  ArduinoOTA.onEnd([]()
-                   { Serial.println("\nEnd"); });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
-  ArduinoOTA.onError([](ota_error_t error)
-                     {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
-  ArduinoOTA.begin();
+  initializeOTA();
+
   Serial.println("Ready");
 
   Subject subject;
@@ -86,6 +77,7 @@ void setup()
 
   while (true)
   {
+    handleOTA();
     Serial.println("Measurement started");
     float cppm = gasSensor.getCorrectedPPM(DEFAULT_TEMP, DEFAULT_HUM);
     if (isUseHygro)
